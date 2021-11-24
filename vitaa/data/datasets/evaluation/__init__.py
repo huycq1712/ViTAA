413e8e2913cb4ea758e92e65ba2dfcdf61e42aac
Huy Q Can <huysk82000@gmail.com>
2021-11-24 19:23:41 +0700

    update something about vnps3k dataset

--- vitaa/data/datasets/evaluation/__init__.py
+++ vitaa/data/datasets/evaluation/__init__.py
@@ -1,8 +1,8 @@
 from vitaa.data import datasets
 
-from .cuhkpedes import cuhkpedes_evaluation
-from .market1501 import market1501_evaluation
-from .dukemtmc import dukemtmc_evaluation
+from .vnpersonsearch3k import vnpersonsearch3k_evaluation
+#from .market1501 import market1501_evaluation
+#from .dukemtmc import dukemtmc_evaluation
 
 
 def evaluate(dataset, predictions, output_folder):
@@ -20,13 +20,13 @@ def evaluate(dataset, predictions, output_folder):
         dataset=dataset, predictions=predictions, output_folder=output_folder
     )
     if isinstance(dataset, datasets.CUHKPEDESDataset):
-        return cuhkpedes_evaluation(**args)
+        return vnpersonsearch3k_evaluation(**args)
     elif isinstance(dataset, datasets.Market1501Dataset):
-        return market1501_evaluation(**args)
+        return vnpersonsearch3k_evaluation(**args)
     elif isinstance(dataset, datasets.DukeMTMCDataset):
-        return dukemtmc_evaluation(**args)
+        return vnpersonsearch3k_evaluation(**args)
     elif isinstance(dataset, datasets.DemoDataset):
-        return cuhkpedes_evaluation(**args)
+        return vnpersonsearch3k_evaluation(**args)
     else:
         dataset_name = dataset.__class__.__name__
         raise NotImplementedError("Unsupported dataset type {}.".format(dataset_name))
